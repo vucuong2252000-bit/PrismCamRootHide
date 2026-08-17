0c1d4b5324bee5192ea1c1bd01befa15f5d64765
vucuong2252000-bit <vucuong2252000@gmail.com>
2026-08-17 17:09:38 +0700

    Update PCCameraProcessor.h

--- camera/PCCameraProcessor.h
+++ camera/PCCameraProcessor.h
@@ -1,5 +1,13 @@
 #import <Foundation/Foundation.h>
 #import <CoreMedia/CoreMedia.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void PCProcessCameraSample(CMSampleBufferRef sampleBuffer);
 void PCReportHookState(NSString *message, BOOL installed);
+
+#ifdef __cplusplus
+}
+#endif
